71ef2d2d9f7ce9cbddd7e3939d4c0519453d0f25
gabrielestefono <gabrielestefono@hotmail.com>
2023-04-21 18:36:10 -0300

    Aula 02

---
+++ Professor_Dimitri/Aula03/js/index.js
@@ -0,0 +1,4 @@
+let paragrafo = document.getElementById("texto");
+paragrafo.innerHTML = "Meu primeiro texto <b>JavaScript</b>";
+alert("Isso é um alerta!");
+console.log("Tô invisível!")
---
+++ Professor_Ivan/curso_js/js/script2.js
@@ -0,0 +1,8 @@
+// Este é um comentário antes de um alert
+// alert("Mensagem enviada pelo alert!");
+/* Este
+comentário
+tem
+várias
+linhas */
+console.log("Mensagem enviada pelo console.log");
