38d8ab5a6efeee3ce444113d95165d2d1e3009ad
gabrielestefono <gabrielestefono@hotmail.com>
2023-04-25 11:55:15 -0300

    Aula 05

---
+++ Professor_Dimitri/Aula06/js/index.js
@@ -0,0 +1,57 @@
+//var pote = "Bombom";
+//console.log(pote);
+
+// var a = 2;
+// var b = 3;
+// var c = a +  b;
+// console.log(c);
+
+// // Declaração de variáveis.
+// var a, b, c;
+// // Atribuição de meu código
+// a = 2;
+// b = 3;
+// c = a + b;
+// console.log(c);
+
+// var c;
+// console.log(c); // undefined
+
+// var a, b, c;
+// c = a + b;
+// console.log(c) // NaN
+
+// var a = 2;
+// var a = 5;
+// console.log(a);
+
+// var nome = "Gabriel";
+// var sobrenome = "Estéfono";
+// var idade = 26;
+// var nomeCompleto = nome + sobrenome;
+// console.log(nomeCompleto);
+// nomeCompleto = nome + " " + sobrenome;
+// console.log(nomeCompleto);
+// var pessoa = idade + nome;
+// console.log(pessoa);
+
+// let pessoa = "Gabriel";
+// let pessoa = "João"; // Não funciona, retorna erro
+
+// var x = 10;
+// {
+//     var x = 20;
+// }
+
+// console.log(x);
+
+// let y = 5;
+// {
+//     let y = 10;
+// }
+
+// console.log(y);
+
+// const valor = 5;
+// valor = 10;
+// console.log(valor); // Retorna erro
---
+++ Professor_Mateus/Secao02/05-MaisSobreStrings/script.js
@@ -0,0 +1,7 @@
+console.log("Primeira linha \nSegunda linha");
+console.log("O meu nome é 'Mateus'");
+console.log("O nome dela é 'Roberta'");
+console.log("O nome dela é \"Roberta\"");
+console.log("O meu nome é \"Mateus\"");
+console.log(`A multiplicação de 5 por 3 é ${5*3}`);
+console.log("O " + "meu " + "nome " + "é " + "'Gabriel'");
