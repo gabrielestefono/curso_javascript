9f227011ac71aaa119e231789e4c288ff80d5b2f
gabrielestefono <gabrielestefono@hotmail.com>
2023-05-02 07:38:39 -0300

    Aula 08 - Mateus Batisti

---
+++ Professor_Mateus/Secao02/07-ComparacoesBooleans/script.js
@@ -0,0 +1,17 @@
+// maior que >
+// menor que <
+// maior ou igual >=
+// menor ou igual <=
+// igual ==
+// diferente !=
+// idêntico ===
+// Não idêntico !==
+
+console.log(1>2);
+console.log(5<10);
+console.log(3 >= 3);
+console.log(5<=4);
+console.log(5 == 4);
+console.log('Gabriel' != 'Gabriel');
+console.log(3 === '3');
+console.log(4 === 4);
