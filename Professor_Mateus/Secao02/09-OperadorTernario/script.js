b2568b60c8169fd1b76fdbadb426882df38ca880
gabrielestefono <gabrielestefono@hotmail.com>
2023-05-06 10:42:46 -0300

    Aula 09 - Mateus Batisti

---
+++ Professor_Mateus/Secao02/09-OperadorTernario/script.js
@@ -0,0 +1,5 @@
+console.log(true ? 1 : 2);
+console.log(false ? 1 : 2);
+console.log(true ? 'Verdadeiro' : 'Falso');
+console.log(false ? 'Verdadeiro' : 'Falso');
+console.log('Gabriel' == 'Gabriel' ? 'Fala aí, Gabriel' : 'Não é o Gabriel');
