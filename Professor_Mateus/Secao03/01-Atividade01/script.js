1a73d3ece7c73720809885691cd092f9879f1fd5
gabrielestefono <gabrielestefono@hotmail.com>
2023-05-10 08:33:49 -0300

    Aula 12 - Mateus Batisti

---
+++ Professor_Mateus/Secao03/01-Atividade01/script.js
@@ -0,0 +1,7 @@
+console.log('Olá, meu nome é Gabriel');
+console.log("Olá, meu nome é Gabriel");
+console.log(`Olá, meu nome é Gabriel`);
+
+console.log(typeof('Olá, meu nome é Gabriel'));
+console.log(typeof("Olá, meu nome é Gabriel"));
+console.log(typeof(`Olá, meu nome é Gabriel`));
