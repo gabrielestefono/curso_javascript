0b6ed72ce5ec2c6cb9feb5f4c8c6de09eb20eafc
gabrielestefono <gabrielestefono@hotmail.com>
2023-05-20 16:26:08 -0300

    MB. Seção 04 - Aula 01

---
+++ Professor_Mateus/Secao04/01_variaveis/script.js
@@ -0,0 +1,15 @@
+let nome = 'Gabriel';
+
+let laranjas = 5;
+laranjas = 15546;
+
+nome = 'João'
+
+console.log(`o meu nome é ${nome}`)
+console.log(laranjas * laranjas);
+
+
+
+let um, dois, tres;
+um = 1, dois = 2, tres = 3;
+console.log(um + dois + tres);
