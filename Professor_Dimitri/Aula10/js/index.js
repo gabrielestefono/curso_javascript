b203d1be3710483fbb35e1d203354c7bf386546d
gabrielestefono <gabrielestefono@hotmail.com>
2023-05-22 22:22:58 -0300

    DY. AULA 10

---
+++ Professor_Dimitri/Aula10/js/index.js
@@ -0,0 +1,19 @@
+function clique(){
+    document.body.style.backgroundColor = "Yellow";
+}
+
+function duploClique(){
+    document.body.style.backgroundColor = "green";
+}
+
+function mouseEmCima(){
+    document.body.style.backgroundColor = "red";
+}
+
+function mouseSai(){
+    document.body.style.backgroundColor = "blue";
+}
+
+function mouseSeMove(){
+    document.getElementById('texto').innerHTML('Se moveu')
+}
