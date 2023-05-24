c32d04859d0cc3130abbf77727f983d14a398620
gabrielestefono <gabrielestefono@hotmail.com>
2023-05-24 10:05:38 -0300

    DY. Aula 12

---
+++ Professor_Dimitri/Aula12/js/index.js
@@ -0,0 +1,14 @@
+const pessoa = ["Dimitri", "Teixeira", 30, "Professor"];
+let teste = document.getElementById('teste')
+// document.getElementById('teste').innerText = pessoa.join(' - ');
+// pessoa.pop();
+// pessoa.push("Qualquer coisa");
+// pessoa.shift(); 
+// pessoa.unshift();
+// pessoa.splice();
+
+
+//slice corta strings ou arrays
+// Sort - Organiza as arrays
+
+teste.innerHTML = pessoa
