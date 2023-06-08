f5c12af1a6fc71f37bc5f4c721d7d5f40d0ed698
gabrielestefono <gabrielestefono@hotmail.com>
2023-06-07 22:30:12 -0300

    MB. Seção 04 - Aula 03

---
+++ Professor_Mateus/Secao04/03_convencao/script.js
@@ -0,0 +1,9 @@
+// let 3nome =  'Gabriel'; Não pode, ocorre um erro!
+let nome3 = 'Gabriel';
+let $nome = 'Gabriel';
+let _nome = 'Gabriel';
+
+// Sempre inicie com letra, $ ou _;
+// Usar camelcase
+
+sinalDaCrus = "Amém!";
