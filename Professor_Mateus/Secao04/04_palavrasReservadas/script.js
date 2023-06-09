f386813a2659b8008fc9a0d3f2f217746f8f1bf6
gabrielestefono <gabrielestefono@hotmail.com>
2023-06-08 22:34:29 -0300

    MB. Seção 04 - Aula 04

---
+++ Professor_Mateus/Secao04/04_palavrasReservadas/script.js
@@ -0,0 +1,7 @@
+/* 
+Existem algumas palavras que são reservadas pelo sistema, assim, 
+não é possível criar variáveis com essas palavras, ou pelo menos não é recomendado que se utilize.
+*/
+
+// let if = 'Meu nome'; // Ocorre erro.
+// let function = 'Sim'; // Ocorre erro.
