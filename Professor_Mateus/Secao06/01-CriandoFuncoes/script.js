5620698e2e4da9ddae605ba5d56ef55b5c4111f7
gabrielestefono <gabrielestefono@hotmail.com>
2023-06-19 05:25:46 -0300

    MB. Seção 06 - Aula 01

---
+++ Professor_Mateus/Secao06/01-CriandoFuncoes/script.js
@@ -0,0 +1,17 @@
+function imprimirNoConsole(){
+    console.log('Impresso');
+}
+
+imprimirNoConsole();
+
+function imprimirUmNumero(num){
+    console.log("O número é: " + num);
+}
+
+imprimirUmNumero(2);
+
+const numeroAleatorio = () =>{
+    console.log(Math.random());
+}
+
+numeroAleatorio();
